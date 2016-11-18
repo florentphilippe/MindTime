e26d709820679f576ec3f5bce0f5d91daab811a7
Florent Philippe <florent.philippe17@gmail.com>
2016-11-18 18:05:04 +0100

    Improving the secondary window with types choiceBox

--- src/Main/newEventWindow.java
+++ src/Main/newEventWindow.java
@@ -3,6 +3,7 @@ package Main;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -20,6 +21,15 @@ public class newEventWindow {
         Label nameLabel = new Label("Name : ");
         GridPane.setConstraints(nameLabel,0,0);
 
+        Label typeLabel = new Label("Type : ");
+        GridPane.setConstraints(typeLabel,0,1);
+
+        //ChoiceBox
+        ChoiceBox<String> typeChoice = new ChoiceBox<>();
+        typeChoice.getItems().addAll("■ : Task","• : Note ","O : Event");
+        typeChoice.setValue("■ : Task");
+        GridPane.setConstraints(typeChoice,1,1);
+
         //Inputs
         TextField nameInput = new TextField();
         nameInput.setPromptText("New Event");
@@ -30,9 +40,9 @@ public class newEventWindow {
         //Main layout
         GridPane secondaryLayout = new GridPane();
         secondaryLayout.setPadding(new Insets(10,10,10,10));
-        secondaryLayout.setVgap(5);
+        secondaryLayout.setVgap(15);
         secondaryLayout.setHgap(5);
-        secondaryLayout.getChildren().addAll(nameLabel,nameInput);
+        secondaryLayout.getChildren().addAll(nameLabel,nameInput,typeLabel,typeChoice);
 
         //Root scene
         Scene rootScene = new Scene(secondaryLayout,430,300);
@@ -42,5 +52,10 @@ public class newEventWindow {
         secondaryStage.show();
 
         return secondaryStage;
+    }
+
+    //typeChoice getter
+    public static String getTypeChoice(ChoiceBox<String> choiceBox){
+        return choiceBox.getValue();
     }
 }
