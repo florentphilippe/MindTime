085fbb9c4ebf42167b27e4e150d8bd1228080e71
Florent Philippe <florent.philippe17@gmail.com>
2016-11-20 13:30:37 +0100

    Creating the DayEvent class : a list of BulletEvents by day with the Setters and the Getters

---
+++ src/Body/DayEvent.java
@@ -0,0 +1,59 @@
+package Body;
+
+
+import java.time.LocalDate;
+
+public class DayEvent {
+    /*
+    *The list of BulletEvent by days
+    */
+
+    private BulletEvent eventsList[];
+    private LocalDate dateEvents;
+    private Integer uniqueValue;
+
+
+    //Main Constructor
+    public DayEvent(){
+        eventsList = new BulletEvent[100];
+        dateEvents = LocalDate.now();
+        uniqueValue = 00000000;
+    }
+
+    //Parameters Constructor
+    public DayEvent(BulletEvent[] cEvents,LocalDate cLocalDate,Integer cUniqueValue){
+        eventsList = new BulletEvent[100];
+        dateEvents = LocalDate.now();
+        uniqueValue = 00000000;
+    }
+
+    //***Setters***
+    public void setEvents(BulletEvent[] events) {
+        this.eventsList = events;
+    }
+
+    public void setDateEvents(LocalDate dateEvents) {
+        this.dateEvents = dateEvents;
+    }
+
+    public void setUniqueValue(Integer uniqueValue) {
+        this.uniqueValue = uniqueValue;
+    }
+
+
+    //***Getters***
+    public BulletEvent[] getEvents() {
+        return eventsList;
+    }
+
+    public LocalDate getDateEvents() {
+        return dateEvents;
+    }
+
+    public Integer getUniqueValue() {
+        return uniqueValue;
+    }
+
+
+
+}
