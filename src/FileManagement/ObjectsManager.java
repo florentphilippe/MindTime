3d0a66823155d2a6f18030754e85d28ea831b878
Florent Philippe <florent.philippe17@gmail.com>
2016-12-08 19:50:57 +0100

    Changing wrong operators in the Objectlistsorter.

--- src/FileManagement/ObjectsManager.java
+++ src/FileManagement/ObjectsManager.java
@@ -2,6 +2,8 @@ package FileManagement;
 
 
 import Body.BulletEvent;
+import com.sun.deploy.security.DeployURLClassPathCallback;
+
 import java.io.*;
 import java.util.ArrayList;
 
@@ -45,32 +47,31 @@ public class ObjectsManager  {
     //BulletEvent list sorter
     public static void BulletEventSorter(ArrayList<BulletEvent> sourceList, BulletEvent bulletEvent){
         ArrayList finalList = new ArrayList();        //Destination list
-        Integer i = 0;          //Loop variable
+        Integer elementInList = 0;          //Loop variable
         Integer y = sourceList.size();
 
-        if (y == 0)
+        if (y == 0) {
             finalList.add(bulletEvent);
-
+            elementInList++;
+        }
         else {
-            while (i <= y ) {
-
-                if (bulletEvent.getUniqueValue() <= sourceList.get(i).getUniqueValue() || bulletEvent.getUniqueValue() > sourceList.get(i).getUniqueValue()) {
+            while (elementInList <= y ) {
+                if (bulletEvent.getUniqueValue() <= sourceList.get(elementInList).getUniqueValue() && bulletEvent.getUniqueValue() > sourceList.get(elementInList).getUniqueValue()) {
                     finalList.add(bulletEvent);
-                    i++;
                     break;
                 }
                 else {
-                    finalList.add(sourceList.get(i));
-                    i++;
+                    finalList.add(sourceList.get(elementInList));
+                    elementInList++;
                 }
             }
-            while (i <= y+1) {
-                finalList.add(sourceList.get(i));
-                i++;
+            while (elementInList <= y) {
+                finalList.add(sourceList.get(elementInList));
+                elementInList++;
             }
         }
         System.out.println("Final list size = "+finalList.size());
-        System.out.println("Number of loop : "+i);
+        System.out.println("Number of loop : "+elementInList);
         sourceList.clear();
         sourceList.addAll(finalList);
     }
